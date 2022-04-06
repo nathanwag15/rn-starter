fad0ae64e1c459dda30ae7fe3991849f666c42a2
Nate Wagstaff <natewagstaff21@gmail.com>
2022-04-05 23:21:58 -0600

    second commit

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}>This is the components screen</Text>
+    const greeting = <Text>Hi there!</Text>;
+    const name = "Nate";
+
+    return (
+        <View>
+            <Text style={styles.textStyle}>Getting Started with React Native!</Text>
+            <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+        </View>
+    );        
 };
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
+    },
+    subHeaderStyle: {
+        fontSize: 20
     }
 });
 
-export default ComponentsScreen;
+export default ComponentsScreen;``
